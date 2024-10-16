d31b16b1975c8c270deb5aea98ac705cf97680da
Vinícius Valle Beraldo <vvberaldo@proton.me>
2024-10-15 22:36:49 -0300

    enviando as páginas html para fazer login ou registrar-se

---
+++ src/Views/login.php
@@ -0,0 +1,27 @@
+<div class="container">
+    <h1>Login</h1>
+    <main>
+        <form class="needs-validation" action="" method="post">
+            <div class="row g-3">
+                <div class="col-sm-6">
+                    <label for="subject" class="form-label">subject</label>
+                    <input type="text" class="form-control" name="subject" id="subject" placeholder="" value="" >
+                </div>
+                <div class="col-6">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="text" class="form-control" name="email" id="email" placeholder="you@example.com">
+                </div>
+                <div class="col-6">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" name="password" id="password">
+                </div>
+                <hr class="my-4">
+                <div class="col-12">
+                    <label for="body" class="form-label">Body</label>
+                    <textarea class="form-control" id="body" name="body" ></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </form>
+    </main>
+</div>
---
+++ src/Views/register.php
@@ -0,0 +1,35 @@
+<div class="container">
+    <h1>Create an account</h1>
+    <main>
+        <form class="needs-validation" action="" method="post">
+            <div class="row g-3">
+                <div class="col-sm-3">
+                    <label for="subject" class="form-label">Firstname</label>
+                    <input type="text" class="form-control" name="fistname" id="firstname" placeholder="" value="" >
+                </div>
+                <div class="col-sm-3">
+                    <label for="subject" class="form-label">Lastname</label>
+                    <input type="text" class="form-control" name="lastname" id="lastname" placeholder="" value="" >
+                </div>
+                <div class="col-3">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="text" class="form-control" name="email" id="email" placeholder="you@example.com">
+                </div>
+                <div class="col-6">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" name="password" id="password">
+                </div>
+                <div class="col-6">
+                    <label for="password" class="form-label">Password Repeat</label>
+                    <input type="password" class="form-control" name="cofirmPassword" id="confirmPassword">
+                </div>
+                <hr class="my-4">
+                <div class="col-12">
+                    <label for="body" class="form-label">Body</label>
+                    <textarea class="form-control" id="body" name="body" ></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </form>
+    </main>
+</div>
