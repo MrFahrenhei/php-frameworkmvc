afb25f3e8030ae5636ccff084bbe2175ac1b4e73
Vinícius Valle Beraldo <vvberaldo@proton.me>
2024-10-17 21:59:47 -0300

    colocado de maneira padrão o main como layout principal e criado a funcão para alterar o valor do layout

--- src/Core/Controller.php
+++ src/Core/Controller.php
@@ -9,6 +9,11 @@ namespace App\Core;
  */
 class Controller
 {
+    public string $layout = 'main';
+    public function setLayout(string $layout)
+    {
+       $this->layout = $layout;
+    }
     public function render($view, $params = [])
     {
         return Application::$app->router->renderView($view, $params);
