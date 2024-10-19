8460e73652a040c4389828135391dfba3864fe1d
Vinícius Valle Beraldo <vvberaldo@proton.me>
2024-10-19 13:52:10 -0300

    iniciando as variáveis públicas e arrumando o nome da variável passwordConfirm para confirmPassword

--- src/Models/RegisterModel.php
+++ src/Models/RegisterModel.php
@@ -13,15 +13,15 @@ use App\Core\Model;
 
 class RegisterModel extends Model
 {
-    public string $firstname;
-    public string $lastname;
-    public string $email;
-    public string $password;
-    public string $passwordConfirm;
+    public string $firstname = '';
+    public string $lastname = '';
+    public string $email = '';
+    public string $password = '';
+    public string $confirmPassword = '';
 
-    public function register()
+    public function register(): string
     {
-       echo 'Creating new User';
+       return 'Creating new User';
     }
     public function rules(): array
     {
@@ -30,7 +30,7 @@ class RegisterModel extends Model
             'lastname' => [self::RULE_REQUIRED],
             'email' => [self::RULE_REQUIRED, self::RULE_EMAIL],
             'password' => [self::RULE_REQUIRED, [self::RULE_MIN, 'min' => 8, self::RULE_MAX, 'max'=>30]],
-            'passwordConfirm' => [self::RULE_REQUIRED, [self::RULE_MATCH, 'match'=>'password']],
+            'confirmPassword' => [self::RULE_REQUIRED, [self::RULE_MATCH, 'match'=>'password']],
         ];
     }
 }
