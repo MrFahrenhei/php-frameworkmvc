e11dcf680d47ca6696a25cd3ab98023cc7b3d72c
Vinícius Valle Beraldo <vvberaldo@proton.me>
2024-10-19 13:53:33 -0300

    apagando o html e deixando só as funções para renderizar o formulário

--- src/Views/register.php
+++ src/Views/register.php
@@ -1,31 +1,23 @@
+<?php
+
+use App\Core\Form\Form;
+
+?>
 <div class="container">
     <h1>Create an account</h1>
-    <main>
-        <form class="needs-validation" action="" method="post">
-            <div class="row g-3">
-                <div class="col-sm-3">
-                    <label for="subject" class="form-label">Firstname</label>
-                    <input type="text" class="form-control" name="firstname" id="firstname" placeholder="" value="" >
-                </div>
-                <div class="col-sm-3">
-                    <label for="subject" class="form-label">Lastname</label>
-                    <input type="text" class="form-control" name="lastname" id="lastname" placeholder="" value="" >
-                </div>
-                <div class="col-3">
-                    <label for="email" class="form-label">Email</label>
-                    <input type="text" class="form-control" name="email" id="email" placeholder="you@example.com">
-                </div>
-                <div class="col-6">
-                    <label for="password" class="form-label">Password</label>
-                    <input type="password" class="form-control" name="password" id="password">
-                </div>
-                <div class="col-6">
-                    <label for="password" class="form-label">Password Repeat</label>
-                    <input type="password" class="form-control" name="passwordConfirm" id="confirmPassword">
-                </div>
-                <hr class="my-4">
-                <button type="submit" class="btn btn-primary">Submit</button>
+        <?php $form = Form::begin('', 'POST') ?>
+        <div class="row">
+            <div class="col">
+                <?= $form->field($model, 'firstname') ?>
             </div>
-        </form>
-    </main>
+            <div class="col">
+                <?= $form->field($model, 'lastname') ?>
+            </div>
+        </div>
+        <?= $form->field($model, 'email')->emailField() ?>
+        <?= $form->field($model, 'password')->passwordField() ?>
+        <?= $form->field($model, 'confirmPassword')->passwordField() ?>
+        <hr class="my-4">
+        <button type="submit" class="btn btn-primary">Submit</button>
+        <?php echo Form::end() ?>
 </div>
