bf1901299b0a4c5188045eeff199d7978c6c230b
Vinícius Valle Beraldo <vvberaldo@proton.me>
2024-10-20 11:15:04 -0300

    criado a regra para manter o email único, o status do usuário e uma função que salva no db que ao mesmo tempo que criptografa a senha

--- src/Models/User.php
+++ src/Models/User.php
@@ -14,24 +14,42 @@ use App\Core\Model;
 
 class User extends DBModel
 {
+    private const STATUS_INACTIVE = 0;
+    private const STATUS_ACTIVE = 1;
+    private const STATUS_DELETED = 2;
     public string $firstname = '';
     public string $lastname = '';
     public string $email = '';
+    public int $status = self::STATUS_INACTIVE;
     public string $password = '';
     public string $confirmPassword = '';
 
-    public function register(): string
+    public function tableName(): string
     {
-       return 'Creating new User';
+       return 'users';
+    }
+    public function save(): string
+    {
+        $this->status = self::STATUS_INACTIVE;
+        $this->password = password_hash($this->password, PASSWORD_DEFAULT);
+        return parent::save();
     }
     public function rules(): array
     {
         return [
             'firstname' => [self::RULE_REQUIRED],
             'lastname' => [self::RULE_REQUIRED],
-            'email' => [self::RULE_REQUIRED, self::RULE_EMAIL],
+            'email' => [self::RULE_REQUIRED, self::RULE_EMAIL, [
+                self::RULE_UNIQUE,
+                'class' => self::class,
+            ]],
             'password' => [self::RULE_REQUIRED, [self::RULE_MIN, 'min' => 8, self::RULE_MAX, 'max'=>30]],
             'confirmPassword' => [self::RULE_REQUIRED, [self::RULE_MATCH, 'match'=>'password']],
         ];
+    }
+
+    public function attributes(): array
+    {
+       return ['firstname', 'lastname', 'email', 'password', 'status'];
     }
 }
