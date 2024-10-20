cf0264be1c96d175fbbc4f9f55b478d9927042bc
Vinícius Valle Beraldo <vvberaldo@proton.me>
2024-10-20 09:45:04 -0300

    criando as migrações de db

---
+++ src/Migrations/m0001_initinal.php
@@ -0,0 +1,29 @@
+<?php
+
+class m0001_initinal{
+    public function up()
+    {
+        $db = \App\Core\Application::$app->db;
+        $sql = '
+                CREATE TABLE IF NOT EXISTS 
+                `users` 
+                (`id` int AUTO_INCREMENT PRIMARY KEY,
+                `email` varchar(255) NOT NULL,
+                firstname varchar(255) NOT NULL,
+                lastname varchar(255) NOT NULL,
+                status tinyint(1) NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                )
+                ENGINE=InnoDB 
+                DEFAULT CHARSET=utf8;
+        ';
+        $db->pdo->exec($sql);
+    }
+
+    public function down()
+    {
+        $db = \App\Core\Application::$app->db;
+        $sql = 'DROP TABLE `users`';
+        $db->pdo->exec($sql);
+    }
+}
