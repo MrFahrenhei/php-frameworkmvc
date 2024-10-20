0caea05cfc0572d85d2fd2ec65733c3a1546ec6c
Vinícius Valle Beraldo <vvberaldo@proton.me>
2024-10-20 13:28:13 -0300

    Instanciando a classe Session no __construct da aplicação

--- src/Core/Application.php
+++ src/Core/Application.php
@@ -18,6 +18,7 @@ class Application
     public readonly Request $request;
     public readonly Response $response;
     public readonly Database $db;
+    public Session $session;
     public static Application $app;
     public Controller $controller;
     public function __construct(
@@ -29,6 +30,7 @@ class Application
         self::$app = $this;
         $this->request = new Request();
         $this->response = new Response();
+        $this->session = new Session();
         $this->router = new Router($this->request, $this->response);
 
         $this->db = new Database($config['db']);
