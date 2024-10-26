a0c229c3b1534ab83e666e5449fd26e8134010a5
Vinícius Valle Beraldo <vvberaldo@proton.me>
2024-10-26 13:48:52 -0300

    criando o form do contact

--- src/Views/contact.php
+++ src/Views/contact.php
@@ -1,26 +1,23 @@
+<?php
+use App\Core\Form\Form;
+use App\Core\Form\TextAreaField;
+use App\Core\View;
+use App\Models\ContactForm;
+
+/**
+ * @var $this View
+ * @var $model ContactForm
+ */
+
+$this->title = 'Contact';
+?>
+<h1>Contact Us</h1>
 <div class="container">
-    <main>
-        <form class="needs-validation" action="" method="post">
-            <div class="row g-3">
-                <div class="col-sm-6">
-                    <label for="subject" class="form-label">subject</label>
-                    <input type="text" class="form-control" name="subject" id="subject" placeholder="" value="" >
-                </div>
-                <div class="col-6">
-                    <label for="email" class="form-label">Email</label>
-                    <input type="text" class="form-control" name="email" id="email" placeholder="you@example.com">
-                </div>
-                <div class="col-6">
-                    <label for="password" class="form-label">Password</label>
-                    <input type="password" class="form-control" name="password" id="password">
-                </div>
-                <hr class="my-4">
-                <div class="col-12">
-                    <label for="body" class="form-label">Body</label>
-                    <textarea class="form-control" id="body" name="body" ></textarea>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </div>
-        </form>
-    </main>
+    <?php $form = Form::begin('', 'POST'); ?>
+    <?= $form->field($model, 'subject'); ?>
+    <?= $form->field($model, 'email'); ?>
+    <?= new TextAreaField($model, 'body'); ?>
+    <hr class="my-4">
+    <button type="submit" class="btn btn-primary">Submit</button>
+    <?= Form::end(); ?>
 </div>
