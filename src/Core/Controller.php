c0535579228a624a189fa78373ea9e5e8d0143c5
Vinícius Valle Beraldo <vvberaldo@proton.me>
2024-10-26 13:37:37 -0300

    colocando o retorno com PHPDocs e trocado a função que renderiza o view usando o metodo da classe View

--- src/Core/Controller.php
+++ src/Core/Controller.php
@@ -17,18 +17,38 @@ class Controller
      * @var BaseMiddleware[]
      */
     protected array $middleware = [];
+
+    /**
+     * @param string $layout
+     * @return void
+     */
     public function setLayout(string $layout): void
     {
        $this->layout = $layout;
     }
-    public function render($view, $params = []): false|array|string
+
+    /**
+     * @param string $view
+     * @param array $params
+     * @return false|array|string
+     */
+    public function render(string $view, array $params = []): false|array|string
     {
-        return Application::$app->router->renderView($view, $params);
+        return Application::$app->view->renderView($view, $params);
     }
+
+    /**
+     * @param BaseMiddleware $middleware
+     * @return void
+     */
     public function registerMiddleware(BaseMiddleware $middleware): void
     {
         $this->middleware[] = $middleware;
     }
+
+    /**
+     * @return BaseMiddleware[]
+     */
     public function getMiddleware(): array
     {
         return $this->middleware;
