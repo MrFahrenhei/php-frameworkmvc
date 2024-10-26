421d5a05b068357b37a6707c0282da8510732d71
Vinícius Valle Beraldo <vvberaldo@proton.me>
2024-10-26 13:40:04 -0300

    removendo as funções que trabalham com renderização de páginas para a classe View e colocado também comentários PHPDocs

--- src/Core/Router.php
+++ src/Core/Router.php
@@ -2,7 +2,9 @@
 
 namespace App\Core;
 
-use App\Core\Exception\NotFoundException;/**
+use App\Core\Exception\NotFoundException;
+
+/**
  * Class Router
  *
  * @autor Vinícius Valle Beraldo <vvberaldo@proton.me>
@@ -13,18 +15,40 @@ use App\Core\Exception\NotFoundException;/**
 class Router
 {
     protected array $routes = [];
+
+    /**
+     * @param Request $request
+     * @param Response $response
+     */
     public function __construct(
         public readonly Request $request,
         public readonly Response $response
     ){}
+
+    /**
+     * @param $path
+     * @param $callback
+     * @return void
+     */
     public function get($path, $callback): void
     {
         $this->routes['get'][$path] = $callback;
     }
+
+    /**
+     * @param $path
+     * @param $callback
+     * @return void
+     */
     public function post($path, $callback): void
     {
         $this->routes['post'][$path] = $callback;
     }
+
+    /**
+     * @return mixed
+     * @throws NotFoundException
+     */
     public function resolve(): mixed
     {
         $path = $this->request->getPath();
@@ -36,7 +60,7 @@ class Router
             throw new NotFoundException();
         }
         if (is_string($callback)) {
-            return $this->renderView($callback);
+            return Application::$app->view->renderView($callback);
         }
         if (is_array($callback)) {
             /** @var Controller $controller */
@@ -49,35 +73,5 @@ class Router
             }
         }
         return call_user_func($callback, $this->request, $this->response);
-    }
-    public function renderView($view, $params = []): array|false|string
-    {
-        $layoutContent = $this->layoutContent();
-        $viewContent = $this->renderOnlyView($view, $params);
-        return str_replace('{{content}}', $viewContent, $layoutContent);
-    }
-    protected function renderContent(string $viewContent): array|false|string
-    {
-        $layoutContent = $this->layoutContent();
-        return str_replace('{{content}}', $viewContent, $layoutContent);
-    }
-    protected function layoutContent(): false|string
-    {
-        $layout = Application::$app->layout;
-        if(Application::$app->controller) {
-            $layout = Application::$app->controller->layout;
-        }
-        ob_start();
-        include_once Application::$ROOT_DIR . "/src/Views/layouts/{$layout}.php";
-        return ob_get_clean();
-    }
-    protected function renderOnlyView(string $view, array $params): false|string
-    {
-        foreach($params as $key => $value) {
-            $$key  = $value;
-        }
-        ob_start();
-        include_once Application::$ROOT_DIR . "/src/Views/{$view}.php";
-        return ob_get_clean();
     }
 }
